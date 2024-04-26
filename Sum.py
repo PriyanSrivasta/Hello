fd880892722340086db056ae7c808ffe2a8324f1
PriyanSrivasta <161520063+PriyanSrivasta@users.noreply.github.com>
2024-04-26 10:38:57 +0530

    Create Sum.py

---
+++ Sum.py
@@ -0,0 +1,11 @@
+def add_numbers(num1, num2):
+    sum = num1 + num2
+    return sum
+
+# Taking input from user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Calling the function and printing the result
+result = add_numbers(num1, num2)
+print("The sum is:", result)
